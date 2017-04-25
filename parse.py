7b60169f29c98c02677a34e0b3b95be1d37fd12f
Ken Koyanagi <ken.koyanagi1@gmail.com>
2017-04-25 10:40:17 -0700

    parse.py preprocesses, cleans and has mongodb import the cleaned CSV

--- parse.py
+++ parse.py
@@ -1,35 +1,57 @@
 
 #!/usr/bin/env python
-"""parse.py: Takes Windows CSV, cleans the data and exports as JSON"""
+"""parse.py: Takes Windows CSV, cleans the data and exports as CSV"""
 
-import json
 import pandas as pd
 import os
 
-os.system('clear')
-print("Starting parse.py")
+def main():
+    os.system('clear')
+    print("Starting parse.py ...")
 
-#CSV needs for be formatted as a Windows CSV, should not be OSX CSV.
-csvFilename = "/Users/kenkoyanagi/Projects/SacBusinesses/testCSV.csv"
+    #File of the CSV
+    csvFilename = "/Users/kenkoyanagi/Projects/SacBusinesses/testCSV.csv"
+    cleanCSV(csvFilename)
 
-#Only relevant fields should be read from the CSV
-fields = ["Account Number", "Business Name", "Business Description", "Application Date", "Business Start Date", "Business Close Date", "Current License Status", "Location City", "Location Zip code"]
-df = pd.read_csv(csvFilename, skipinitialspace=True, usecols=fields)
-
-#We only want the businesses that are in Sacramento
-df = df[df['Location City'] == "SACRAMENTO"]
-
-#Replace NaN for Business Description with "OTHER"
-df['Business Description'] = df['Business Description'].fillna(value="OTHER")
-
-#Snip extended zip codes to just five characters
-df['Location Zip code'] = df['Location Zip code'].map(lambda x: str(x)[:5])
-
-#Output the cleansed data to CSV
-df.to_csv("output.csv", index=False)
+    #Import the data in the mongodb
+    mongoImport("output.csv", "sacbusinesses", "test2")
 
 
-#Check the datafield - for testing small amounts
-print(df)
+def cleanCSV(csvFile):
+    #Only relevant fields should be read from the CSV
+    fields = ["Account Number", "Business Name", "Business Description", "Application Date", "Business Start Date", "Business Close Date", "Current License Status", "Location City", "Location Zip code"]
+    df = pd.read_csv(csvFile, skipinitialspace=True, usecols=fields)
+
+    #We only want the businesses that are in Sacramento
+    df = df[df['Location City'] == "SACRAMENTO"]
+
+    #Replace NaN for Business Description with "OTHER"
+    df['Business Description'] = df['Business Description'].fillna(value="OTHER")
+
+    #Snip extended zip codes to just five characters
+    df['Location Zip code'] = df['Location Zip code'].map(lambda x: str(x)[:5])
+
+    #Output the cleansed data to CSV
+    df.to_csv("output.csv", index=False)
+
+    #Check the datafield - for testing small amounts
+    print(df)
+
+def mongoImport(fileName, database, collection):
+    command = "mongoimport -d " + database  + " -c " + collection + " --type csv --file " + fileName + " --headerline"
+    os.system(command)
+
+
+if __name__ == '__main__':
+    main()
+
+
+
+
+
+
+
+
+
 
 
