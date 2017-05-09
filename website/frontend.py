f9fcf9d941c3c7c37d56695c14ca80f8eef69bb2
maddz131 <maddz131@gmail.com>
2017-05-09 10:34:09 -0700

    added pics w/category names. Created html table of json obj

--- website/frontend.py
+++ website/frontend.py
@@ -7,7 +7,7 @@ from flask import jsonify
 from bson import Binary, Code
 from jinja2 import Template
 from bson.json_util import dumps
-from json2html import *
+from json2table import convert
 import HTML
 
 app = Flask(__name__)
@@ -19,11 +19,13 @@ mongo = PyMongo(app)
 def index():
     test = mongo.db.test4.find({"Business Description":"DRIVER"}) #mongo returns bson
     test2 = dumps(test) #converst bson tojson
-    #json_object = [test2]
-    #build_direction = "LEFT_TO_RIGHT"
-    #table_attributes = {"style" : "width:100%"}
-    #html = convert(json_object, build_direction=build_direction, table_attributes=table_attributes)
     data = json.loads(test2)
-    return render_template('index.html', data=data)#passes json to index.html
+    build_direction = "TOP_TO_BOTTOM"
+    html = ""
+    table_attributes = {"style" : "width:100%", "class" : "table table-striped"}
+    for obj in range(len(data)):
+        json_object = data[obj]
+        html = html+convert(json_object, build_direction=build_direction, table_attributes=table_attributes)
+    return render_template('index.html', html=html)#passes json to index.html
 
 app.run(debug = True, host = '0.0.0.0', port = 5000) #running on localhost:5000
