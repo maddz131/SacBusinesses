1eda82c866029604c4d108f3ca23cc96200916ec
maddz131 <maddz131@gmail.com>
2017-05-09 07:48:28 -0700

    json displaying better

--- website/frontend.py
+++ website/frontend.py
@@ -5,7 +5,10 @@ from flask_pymongo import PyMongo
 from flask import json
 from flask import jsonify
 from bson import Binary, Code
+from jinja2 import Template
 from bson.json_util import dumps
+from json2html import *
+import HTML
 
 app = Flask(__name__)
 #app.config['MONGO_URI'] = 'mongodb://localhost'
@@ -15,7 +18,12 @@ mongo = PyMongo(app)
 @app.route('/')
 def index():
     test = mongo.db.test4.find({"Business Description":"DRIVER"}) #mongo returns bson
-    test2 = dumps(test) #converst bson to json
-    return render_template('index.html', test2=test2)#passes json to index.html
+    test2 = dumps(test) #converst bson tojson
+    #json_object = [test2]
+    #build_direction = "LEFT_TO_RIGHT"
+    #table_attributes = {"style" : "width:100%"}
+    #html = convert(json_object, build_direction=build_direction, table_attributes=table_attributes)
+    data = json.loads(test2)
+    return render_template('index.html', data=data)#passes json to index.html
 
 app.run(debug = True, host = '0.0.0.0', port = 5000) #running on localhost:5000
